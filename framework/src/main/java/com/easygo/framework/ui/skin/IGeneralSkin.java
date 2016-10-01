696576fd5854c0f2c8f3465676814dd354325c67
unknown <280327097@qq.com>
2016-10-01 10:25:18 +0800

    first commoit

---
+++ framework/src/main/java/com/easygo/framework/ui/skin/IGeneralSkin.java
@@ -0,0 +1,15 @@
+package com.easygo.framework.ui.skin;
+
+/**
+ * Project: CCBClient_V4.00<br/>
+ * Package: com.ccb.framework.ui.skin<br/>
+ * ClassName: IGeneralSkin<br/>
+ * Description: 动态换肤接口 <br/>
+ * Date: 2016-7-7 11:39 <br/>
+ */
+public interface IGeneralSkin {
+
+	void onSkinChange();
+
+	boolean isGeneralSkin();
+}
---
+++ framework/src/main/java/com/easygo/framework/ui/skin/SkinColorTool.java
@@ -0,0 +1,10 @@
+package com.easygo.framework.ui.skin;
+
+/**
+ * @author by lyh on 2016/10/1 01:06.
+ *         version 1.0
+ * @description
+ */
+
+public class SkinColorTool {
+}
